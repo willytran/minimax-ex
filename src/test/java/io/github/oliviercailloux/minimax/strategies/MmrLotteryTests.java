182538ea2ff882a4af4dd7b7a54a2787d5a6a8a1
xoxor <beatrice.napolitano@live.it>
2020-09-09 20:18:56 +0200

    Jupiter instead of JUnit

--- src/test/java/io/github/oliviercailloux/minimax/strategies/MmrLotteryTests.java
+++ src/test/java/io/github/oliviercailloux/minimax/strategies/MmrLotteryTests.java
@@ -1,11 +1,10 @@
 package io.github.oliviercailloux.minimax.strategies;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Comparator;
-import java.util.HashMap;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import io.github.oliviercailloux.j_voting.Alternative;
 import io.github.oliviercailloux.j_voting.Voter;
@@ -20,17 +19,17 @@ public class MmrLotteryTests {
 			
 		assertTrue(compMax.compare(MmrLottery.given(10, 8), MmrLottery.given(8, 6))>0);		//max1>max2		
 		assertTrue(compMax.compare(MmrLottery.given(6, 8), MmrLottery.given(10, 6))<0);		//max1<max2 
-		assertTrue(compMax.compare(MmrLottery.given(10, 8), MmrLottery.given(10, 8))==0);	//max1=max2 & min1=min2
-		assertTrue(compMax.compare(MmrLottery.given(10, 8), MmrLottery.given(10, 6))>0);	//max1=max2 & min1>min2 
-		assertTrue(compMax.compare(MmrLottery.given(10, 4), MmrLottery.given(10, 6))<0);	//max1=max2 & min1<min2 
+		assertTrue(compMax.compare(MmrLottery.given(10, 8), MmrLottery.given(8,10))==0);	//max1=max2 & min1=min2
+		assertTrue(compMax.compare(MmrLottery.given(10, 8), MmrLottery.given(6, 10))>0);	//max1=max2 & min1>min2 
+		assertTrue(compMax.compare(MmrLottery.given(10, 4), MmrLottery.given(6, 10))<0);	//max1=max2 & min1<min2 
 		
 		Comparator<MmrLottery> compMin= MmrLottery.MIN_COMPARATOR;
 		
 		assertTrue(compMin.compare(MmrLottery.given(10, 8), MmrLottery.given(8, 6))>0);		//min1>min2	
 		assertTrue(compMin.compare(MmrLottery.given(6, 8), MmrLottery.given(10, 7))<0);		//min1<min2
-		assertTrue(compMin.compare(MmrLottery.given(10, 8), MmrLottery.given(10, 8))==0);	//min1=min2 & max1=max2
-		assertTrue(compMin.compare(MmrLottery.given(10, 6), MmrLottery.given(8, 6))>0);		//min1=min2 & max1>max2
-		assertTrue(compMin.compare(MmrLottery.given(8, 6), MmrLottery.given(10, 6))<0);		//min1=min2 & max1<max2
+		assertTrue(compMin.compare(MmrLottery.given(10, 8), MmrLottery.given(8, 10))==0);	//min1=min2 & max1=max2
+		assertTrue(compMin.compare(MmrLottery.given(10, 6), MmrLottery.given(6,8))>0);		//min1=min2 & max1>max2
+		assertTrue(compMin.compare(MmrLottery.given(8, 6), MmrLottery.given(6,10))<0);		//min1=min2 & max1<max2
 	}
 	
 }
