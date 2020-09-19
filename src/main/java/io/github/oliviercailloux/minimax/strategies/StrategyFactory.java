299d5aa290e40a4474b52f82e654bb33cf083f22
xoxor <beatrice.napolitano@live.it>
2020-09-19 13:48:31 +0200

    server cannot resolve ∞

--- src/main/java/io/github/oliviercailloux/minimax/strategies/StrategyFactory.java
+++ src/main/java/io/github/oliviercailloux/minimax/strategies/StrategyFactory.java
@@ -107,7 +107,7 @@ public class StrategyFactory implements Supplier<Strategy> {
 		final String prefix = ", constrained to [";
 		final String suffix = "]";
 		final String constraintsDescription = constraints.stream()
-				.map(c -> (c.getNumber() == Integer.MAX_VALUE ? "∞" : c.getNumber())
+				.map(c -> (c.getNumber() == Integer.MAX_VALUE ? "-" : c.getNumber())
 						+ (c.getKind() == QuestionType.COMMITTEE_QUESTION ? "c" : "v"))
 				.collect(Collectors.joining(", ", prefix, suffix));
 
