66ee3d9854bc5f046dd6e871794c3c154fcbdd0f
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-09-28 23:46:05 +0200

    Revert "Complexified StrategyXp"
    This reverts commit fe101f3cc42968c8f784888676d9f9460161a146.

--- src/main/java/io/github/oliviercailloux/minimax/experiment/StrategyXp.java
+++ src/main/java/io/github/oliviercailloux/minimax/experiment/StrategyXp.java
@@ -5,20 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableList;
 
-import io.github.oliviercailloux.minimax.elicitation.Oracle;
 import io.github.oliviercailloux.minimax.elicitation.QuestionType;
 import io.github.oliviercailloux.minimax.experiment.json.JsonConverter;
 import io.github.oliviercailloux.minimax.experiment.other_formats.ToCsv;
 import io.github.oliviercailloux.minimax.strategies.QuestioningConstraint;
 import io.github.oliviercailloux.minimax.strategies.StrategyFactory;
-import io.github.oliviercailloux.minimax.utils.Generator;
 
 public class StrategyXp {
 	@SuppressWarnings("unused")
@@ -34,31 +31,24 @@ public class StrategyXp {
 				ImmutableList.of(QuestioningConstraint.of(QuestionType.VOTER_QUESTION, Integer.MAX_VALUE)));
 //		final StrategyFactory factory = StrategyFactory.byMmrs(seed, MmrLottery.MAX_COMPARATOR);
 //		final StrategyFactory factory = StrategyFactory.random();
-
-		final Supplier<Oracle> oracleFactory = () -> Oracle.build(Generator.genProfile(m, n), Generator.genWeights(m));
-
-		final String prefixDescription = factory.getDescription() + ", m = " + m + ", n = " + n + ", k = " + k;
-		runs(factory, oracleFactory, k, 1, prefixDescription);
+		runs(factory, m, n, k, 1);
 	}
 
 	/**
-	 * Repeat (nbRuns times) a run experiment (thus: get an oracle, ask k
+	 * Repeat (nbRuns times) a run experiment (thus: generate an oracle, ask k
 	 * questions).
-	 *
 	 */
-	public static void runs(StrategyFactory factory, Supplier<Oracle> oracleFactory, int k, int nbRuns,
-			String prefixDescription) throws IOException {
+	public static void runs(StrategyFactory factory, int m, int n, int k, int nbRuns) throws IOException {
 		final Path outDir = Path.of("experiments/");
 		Files.createDirectories(outDir);
-		final String prefixTemp = prefixDescription + ", ongoing";
+		final String prefixTemp = factory.getDescription() + ", m = " + m + ", n = " + n + ", k = " + k + ", ongoing";
 		final Path tmpJson = outDir.resolve(prefixTemp + ".json");
 		final Path tmpCsv = outDir.resolve(prefixTemp + ".csv");
 
 		final ImmutableList.Builder<Run> runsBuilder = ImmutableList.builder();
 		LOGGER.info("Started '{}'.", factory.getDescription());
 		for (int i = 0; i < nbRuns; ++i) {
-			final Oracle oracle = oracleFactory.get();
-			final Run run = Runner.run(factory, oracle, k);
+			final Run run = Runner.run(factory, m, n, k);
 			LOGGER.info("Time (run {}): {}.", i, run.getTotalTime());
 			Files.writeString(Path.of("run " + i + ".json"), JsonConverter.toJson(run).toString());
 			runsBuilder.add(run);
@@ -68,7 +58,8 @@ public class StrategyXp {
 			Files.writeString(tmpCsv, ToCsv.toCsv(runs, 1));
 		}
 
-		final String prefix = prefixDescription + ", nbRuns = " + nbRuns;
+		final String prefix = factory.getDescription() + ", m = " + m + ", n = " + n + ", k = " + k + ", nbRuns = "
+				+ nbRuns;
 		final Path outJson = outDir.resolve(prefix + ".json");
 		final Path outCsv = outDir.resolve(prefix + ".csv");
 		Files.move(tmpJson, outJson, StandardCopyOption.REPLACE_EXISTING);
