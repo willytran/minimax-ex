ab99126ec71f99e6ce22334c7e7e6cfd190095e2
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-01-10 00:11:54 +0100

    Reproduce XP extension for debug

--- src/main/java/io/github/oliviercailloux/minimax/experiment/ReproduceXp.java
+++ src/main/java/io/github/oliviercailloux/minimax/experiment/ReproduceXp.java
@@ -4,6 +4,7 @@ import static com.google.common.base.Verify.verify;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,15 @@ import org.slf4j.LoggerFactory;
 import io.github.oliviercailloux.minimax.elicitation.Oracle;
 import io.github.oliviercailloux.minimax.experiment.json.JsonConverter;
 import io.github.oliviercailloux.minimax.strategies.StrategyFactory;
+import io.github.oliviercailloux.minimax.utils.Generator;
 
 public class ReproduceXp {
 	@SuppressWarnings("unused")
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReproduceXp.class);
 
 	public static void main(String[] args) throws Exception {
-		reproduce();
+//		reproduce();
+		readAndAct();
 	}
 
 	public static void reproduce() throws Exception {
@@ -35,4 +38,26 @@ public class ReproduceXp {
 		}
 	}
 
+	public static void generate() throws Exception {
+		final int m = 4;
+		final int n = 4;
+		final int k = 300;
+
+		final long seed = ThreadLocalRandom.current().nextLong();
+		final StrategyFactory factory = StrategyFactory.css(seed);
+
+		final Oracle oracle = Oracle.build(Generator.genProfile(m, n),
+				Generator.genWeightsWithUnbalancedDistribution(m));
+		Files.writeString(Path.of("oracle.json"), JsonConverter.toJson(oracle).toString());
+		Files.writeString(Path.of("factory.json"), JsonConverter.toJson(factory).toString());
+		Runner.run(factory, oracle, k);
+	}
+
+	public static void readAndAct() throws Exception {
+		final int k = 300;
+
+		final Oracle oracle = JsonConverter.toOracle(Files.readString(Path.of("oracle.json")));
+		final StrategyFactory factory = JsonConverter.toFactory(Files.readString(Path.of("factory.json")));
+		Runner.run(factory, oracle, k);
+	}
 }
